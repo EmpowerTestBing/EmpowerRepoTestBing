08a0bd4ecff1c8ab44a0841456e557566e8fb592
Bing Herng Chong <bing.herng.chong@skyline.be>
2023-05-28 15:57:31 +1000

    Add project files.

---
+++ EmpowerBHCdryRun_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerBHCdryRun_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerBHCdryRun_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8C85217E-2F6E-4B63-BCD6-A355D29BD640")]
+[assembly: AssemblyVersion("1.0.0.0")]
